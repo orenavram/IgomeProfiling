7afff41e97507f836066f5dc8c762536fd31d8fd
Oren <orenavram@gmail.com>
2019-12-11 19:29:33 +0200

    Adding more functionality to the auxiliaries module

--- Auxiliaries/pipeline_auxiliaries.py
+++ Auxiliaries/pipeline_auxiliaries.py
@@ -1,3 +1,25 @@
+nnk_table: {str: str} = {"CGT": "R", "CGG": "R", "AGG": "R",
+                         "CTT": "L", "CTG": "L", "TTG": "L",
+                         "TCT": "S", "TCG": "S", "AGT": "S",
+                         "GCT": "A", "GCG": "A",
+                         "GGT": "G", "GGG": "G",
+                         "CCT": "P", "CCG": "P",
+                         "ACT": "T", "ACG": "T",
+                         "CAG": "Q",
+                         "TAG": "q",
+                         "GTT": "V", "GTG": "V",
+                         "AAT": "N",
+                         "GAT": "D",
+                         "TGT": "C",
+                         "GAG": "E",
+                         "CAT": "H",
+                         "ATT": "I",
+                         "AAG": "K",
+                         "ATG": "M",
+                         "TTT": "F",
+                         "TGG": "W",
+                         "TAT": "Y"}
+
 def verify_file_is_not_empty(file_path):
     import logging
     logger = logging.getLogger('main')
@@ -8,3 +30,21 @@ def verify_file_is_not_empty(file_path):
             msg = f'Input file is empty {file_path}'
             logger.error(msg)
             raise RuntimeError(msg)
+
+
+def load_msa(msa_path):
+    """
+    :param msa_path: a path to an MSA file (in FASTA format)
+    :return: a dictionary that maps each header (without ">" and rstriped()) to its corresponding sequence (rstriped())
+             an int that represent the number of sequences
+             an int that represent the length of the alignment
+    """
+    header_to_sequence = {}
+    with open(msa_path) as f:
+        for header in f:
+            if not header.startswith('>'):
+                raise TypeError('Illegal fasta file')
+            # returns header without ">" !
+            header_to_sequence[header[1:].rstrip()] = f.readline().rstrip()
+
+    return header_to_sequence, len(header_to_sequence), len(header_to_sequence[header.rstrip()])
