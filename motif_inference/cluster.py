d72e0716b0fe34fa17fced8fb27893c0cf367c63
Oren <orenavram@gmail.com>
2019-12-18 20:32:31 +0200

    Adjusting the clustering script for the wrapper

--- motif_inference/cluster.py
+++ motif_inference/cluster.py
@@ -1,25 +1,36 @@
 import subprocess
-import os
 import logging
-from Auxiliaries.pipeline_auxiliaries import verify_file_is_not_empty
+import datetime
+import os
+import sys
+if os.path.exists('/groups/pupko/orenavr2/'):
+    src_dir = '/groups/pupko/orenavr2/igomeProfilingPipeline/src'
+else:
+    src_dir = '/Users/Oren/Dropbox/Projects/gershoni/src'
+sys.path.insert(0, src_dir)
 
-def cluster_sequences(fasta_file, output_path, threshhold, word_length, throw_sequences_shorter_than):
+from auxiliaries.pipeline_auxiliaries import verify_file_is_not_empty
+
+
+def cluster_sequences(fasta_file, output_prefix, done_file_path, threshold, word_length, throw_sequences_shorter_than):
 
     verify_file_is_not_empty(fasta_file)
 
+    logger.info(f'{datetime.datetime.now()}: clustering sequences in {fasta_file}')
+
     # TODO: module load CD hit
     cmd = f'cd-hit -i {fasta_file} ' \
-          f'-o {output_path} ' \
-          f'-c {threshhold} ' \
+          f'-o {output_prefix} ' \
+          f'-c {threshold} ' \
           f'-n {word_length} ' \
           f'-l {throw_sequences_shorter_than}'
     logger.info(f'Starting CD-hit. Executed command is:\n{cmd}')
     subprocess.call(cmd, shell=True)
 
     # make sure that there are results and the file is not empty
-    verify_file_is_not_empty(output_path)
+    verify_file_is_not_empty(f'{output_prefix}.clstr')
 
-    with open(f'{os.path.split(output_path)[0]}/done.txt', 'w'):
+    with open(done_file_path, 'w'):
         pass
 
 
@@ -33,7 +44,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('fasta_file', help='A fasta file to collapse for unique sequences and their counts')
     parser.add_argument('output_prefix', help='A file prefix in which the 2 result files will use as an output path')
-    parser.add_argument('--threshhold', default='1', help='Minimal sequence similarity threshold required',
+    parser.add_argument('done_file_path', help='A path to a file that signals that the clustering was finished.')
+    parser.add_argument('--threshold', default='1', help='Minimal sequence similarity threshold required',
                         type=lambda x: float(x) if 0.4 <= float(x) <= 1
                                                 else parser.error(f'CD-hit allows thresholds between 0.4 to 1'))
     parser.add_argument('--word_length', choices=['2', '3', '4', '5'], default='2',
@@ -48,5 +60,5 @@ if __name__ == '__main__':
         logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger('main')
 
-    cluster_sequences(args.fasta_file, args.output_prefix,
-                      args.threshhold, args.word_length, args.discard)
+    cluster_sequences(args.fasta_file, args.output_prefix, args.done_file_path,
+                      args.threshold, args.word_length, args.discard)
