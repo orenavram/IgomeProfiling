ce20ea02d99ab1b457844786812905a1675f34df
Oren <orenavram@gmail.com>
2019-12-23 15:16:29 +0200

    Tell MAFFT that the msa is of aa rather than let it decide by itself (leads to a very annoying bug!)

--- motif_inference/align_sequences.py
+++ motif_inference/align_sequences.py
@@ -10,25 +10,32 @@ sys.path.insert(0, src_dir)
 from auxiliaries.pipeline_auxiliaries import verify_file_is_not_empty
 
 
-def reconstruct_msa(sequences_file_path, output_file_path, done_path):
+def reconstruct_msa(sequences_file_path, output_file_path, done_path, argv='no argv'):
     import subprocess
 
     # TODO: module load mafft..
     # --auto Automatically selects an appropriate strategy from L-INS-i, FFT-NS-i and FFT-NS-2, according to data size.
-    cmd = f'mafft --auto {sequences_file_path} > {output_file_path}'
-    logger.info(f'{datetime.datetime.now()}: Starting MAFFT. Executed command is:\n{cmd}')
+    # --amino tells mafft that's an amino acid msa. If you let it decide by itself, it might wrong on small data sets
+    # as they might look like dna but they are NOT! e.g.,
+    # [orenavr2@powerlogin-be2 test]$ cat /groups/pupko/orenavr2/igomeProfilingPipeline/experiments/test/analysis/motif_inference/17b_03/unaligned_sequences/17b_03_clusterRank_215_uniqueMembers_2_clusterSize_252.81.faa
+    # >seq_235_lib_12_len_12_counts_126.40626975097965
+    # CNTDVACAAPGN
+    # >seq_1112_lib_C8C_len_10_counts_126.40626975097965
+    # CTTACAPVNC
+    cmd = f'mafft --auto --amino {sequences_file_path} > {output_file_path}'
+    logger.fatal(f'{datetime.datetime.now()}: Starting MAFFT. Executed command is:\n{cmd}')
+    print(f'{datetime.datetime.now()}: Starting MAFFT. Executed command is:\n{cmd}')
     subprocess.run(cmd, shell=True)
 
     # make sure that there are results and the file is not empty
     verify_file_is_not_empty(output_file_path)
 
-    with open(done_path, 'w'):
-        pass
+    with open(done_path, 'w') as f:
+        f.write(' '.join(argv) + '\n')
 
 
 if __name__ == '__main__':
-    from sys import argv
-    print(f'Starting {argv[0]}. Executed command is:\n{" ".join(argv)}')
+    print(f'Starting {sys.argv[0]}. Executed command is:\n{" ".join(argv)}')
 
     import argparse
     parser = argparse.ArgumentParser()
@@ -45,7 +52,7 @@ if __name__ == '__main__':
         logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger('main')
 
-    reconstruct_msa(args.sequences_file_path, args.output_file_path, args.done_file_path)
+    reconstruct_msa(args.sequences_file_path, args.output_file_path, args.done_file_path, argv)
 
 
 
