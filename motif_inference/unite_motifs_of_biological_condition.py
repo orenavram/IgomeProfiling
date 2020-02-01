e4ef170746387fa117edf7d24a26c89fac4dc66b
Oren <orenavram@gmail.com>
2020-02-01 12:40:57 +0200

    Limit number of sequences in msa (after motif unification)

--- motif_inference/unite_motifs_of_biological_condition.py
+++ motif_inference/unite_motifs_of_biological_condition.py
@@ -14,7 +14,7 @@ from auxiliaries.pipeline_auxiliaries import verify_file_is_not_empty, get_clust
 
 
 def get_clusters_sequences(motif_inference_output_path, biological_condition, sample_names,
-                           cluster_names, cluster_rank):
+                           cluster_names, cluster_rank, max_number_of_sequences_to_use):
     sample_paths = []
     for sample_name in sample_names:
         sample_paths.append(os.path.join(motif_inference_output_path, sample_name, 'unaligned_sequences'))
@@ -42,17 +42,23 @@ def get_clusters_sequences(motif_inference_output_path, biological_condition, sa
     unified_dict_header2sequence = {header: sequence for sequence, header in unified_dict_sequence2header.items()}
 
     result = ''
-    for header in sorted(unified_dict_header2sequence, key=get_count_from, reverse=True):
+    for i, header in enumerate(sorted(unified_dict_header2sequence, key=get_count_from, reverse=True)):
+        if i == max_number_of_sequences_to_use:
+            break
         result += f'>{header}\n{unified_dict_header2sequence[header]}\n'
 
-    result_file_name = f'{biological_condition}_clusterRank_{cluster_rank}_' \
-                       f'uniqueMembers_{len(unified_dict_header2sequence)}_' \
-                       f'clusterSize_{sum(get_count_from(header) for header in unified_dict_header2sequence):.2f}.faa'
+    number_of_unique_members = len(unified_dict_header2sequence)
+    cluster_size = sum(get_count_from(header) for header in unified_dict_header2sequence)
+    result_file_name = f'{biological_condition}_clusterRank_' \
+                       f'{str(cluster_rank).zfill(4)}_uniqueMembers_' \
+                       f'{"top" if number_of_unique_members >= max_number_of_sequences_to_use else ""}' \
+                       f'{min(number_of_unique_members, max_number_of_sequences_to_use)}_' \
+                       f'clusterSize_{cluster_size:.2f}.faa'
     return result, result_file_name
 
 
 def unite_clusters(motif_inference_output_path, meme_file, biological_condition, sample_names,
-                   output_path, done_path, aln_cutoff, pcc_cutoff,
+                   max_number_of_members_per_cluster, output_path, done_path, aln_cutoff, pcc_cutoff,
                    unite_pssm_script_path='/groups/pupko/orenavr2/gershoni/src/UnitePSSMs/UnitePSSMs', argv='no_argv'):
 
     clusters_to_combine_path = os.path.join(output_path, 'cluster_to_combine.csv')
@@ -78,6 +84,10 @@ def unite_clusters(motif_inference_output_path, meme_file, biological_condition,
     logger.info(f'Sorting clusters by rank...')
     # sort the sublist such that the first one will contain the highest copy number, etc...
     clusters_to_combine.sort(key=lambda clusters: sum(get_cluster_size_from_name(cluster) for cluster in clusters), reverse=True)
+    sorted_clusters_to_combine_path = clusters_to_combine_path.replace('cluster_to_combine', 'sorted_cluster_to_combine')
+    with open(sorted_clusters_to_combine_path, 'w') as f:
+        for cluster_names in clusters_to_combine:
+            f.write(','.join(cluster_names)+'\n')
 
     unaligned_sequences_path = os.path.join(output_path, 'unaligned_sequences')
     os.makedirs(unaligned_sequences_path, exist_ok=True)
@@ -87,7 +97,8 @@ def unite_clusters(motif_inference_output_path, meme_file, biological_condition,
             logger.info(f'Merging sequences of the cluster ranked {cluster_rank}')
 
         clusters_sequences, cluster_file_name = get_clusters_sequences(motif_inference_output_path, biological_condition,
-                                                                       sample_names, clusters_to_combine[cluster_rank], cluster_rank)
+                                                                       sample_names, clusters_to_combine[cluster_rank],
+                                                                       cluster_rank, max_number_of_members_per_cluster)
         with open(os.path.join(unaligned_sequences_path, cluster_file_name), 'w') as f:
             f.write(clusters_sequences)
 
@@ -109,6 +120,8 @@ if __name__ == '__main__':
     parser.add_argument('sample_names', help='Sample names to apply over the "motif unification". More than one '
                                                  'sample name should be separated by commas but no spaces. '
                                                  'For example: 17b_01,17b_03,17b_05')
+    parser.add_argument('max_number_of_members_per_cluster', type=int,
+                        help='How many members (at most) should be taken to each cluster')
     parser.add_argument('output_path', help='A path in which a new subfolder with the united motifs will be written to')
     parser.add_argument('done_file_path', help='A path to a file that signals that the script finished running successfully.')
     parser.add_argument('--aln_cutoff', default='20', help='TODO')  # TODO: what do this param do?
@@ -123,5 +136,5 @@ if __name__ == '__main__':
     logger = logging.getLogger('main')
 
     unite_clusters(args.motif_inference_output_path, args.meme_file_path, args.biological_condition,
-                   args.sample_names.split(','), args.output_path, args.done_file_path,
+                   args.sample_names.split(','), args.max_number_of_members_per_cluster, args.output_path, args.done_file_path,
                    args.aln_cutoff, args.pcc_cutoff, argv=sys.argv)
