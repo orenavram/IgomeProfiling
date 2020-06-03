fc35462c2ecbc7ce7caf9e2ef19476dde80ca8e7
Oren <orenavram@gmail.com>
2020-06-03 12:31:19 +0300

    Adding a single-feature analysis (measure separability for each feature by itself)

--- model_fitting/random_forest.py
+++ model_fitting/random_forest.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import joblib
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.model_selection import cross_val_score, StratifiedKFold, cross_validate
 # from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import plot_roc_curve
 
@@ -64,6 +64,11 @@ def get_hyperparameters_grid():
                    'min_samples_leaf': min_samples_leaf,
                    'bootstrap': bootstrap}
 
+    if os.path.exists('/Users/Oren'):
+        # use all cores when running locally.
+        # does not apply on the cluster (needed to be set as well in the .pbs file)
+        random_grid['n_jobs'] = [-1]
+
     # Use the random grid to search for best hyperparameters
     return random_grid
 
@@ -77,8 +82,9 @@ def sample_configurations(hyperparameters_grid, num_of_configurations_to_sample)
         configurations.append(configuration)
     return configurations
 
+
 def generate_heat_map(df, number_of_features, hits, number_of_samples, output_path):
-    #plt.figure(dpi=3000)
+    # plt.figure(dpi=1000)
     # transform the data for better contrast in the visualization
     if hits:  # hits data
         df = np.log2(df+1)  # pseudo counts
@@ -89,7 +95,7 @@ def generate_heat_map(df, number_of_features, hits, number_of_samples, output_pa
     cm = sns.clustermap(df, cmap="Blues", col_cluster=False, yticklabels=True)
     plt.setp(cm.ax_heatmap.yaxis.get_majorticklabels(), fontsize=150/number_of_samples)
     cm.ax_heatmap.set_title(f"A heat-map of the significance of the top {number_of_features} discriminatory motifs")
-    cm.savefig(f"{output_path}/{number_of_features}.svg", format='svg', bbox_inches="tight")
+    cm.savefig(f"{output_path}.svg", format='svg', bbox_inches="tight")
     plt.close()
 
 
@@ -109,12 +115,12 @@ def plot_error_rate(errors, features, cv_num_of_splits, output_path_dir):
     plt.close()
 
 
-def save_model_features(X, feature_names, sample_names, number_of_features, output_path):
+def save_model_features(X, feature_indexes, feature_names, sample_names, output_path):
     df = pd.DataFrame()
     for i in range(len(feature_names)):
-        df[feature_names[i]] = X[:, i]  # train_data.iloc[:, :number_of_features]
+        df[feature_names[i]] = X[:, feature_indexes[i]]  # train_data.iloc[:, :number_of_features]
     df.set_index(sample_names, inplace=True)
-    df.to_csv(f"{output_path}/Top_{number_of_features}_features.csv")
+    df.to_csv(f"{output_path}.csv")
     return df
 
 
@@ -125,14 +131,13 @@ def generate_roc_curve(X, y, classifier, number_of_features, output_path):
     plt.close()
 
 
-def train(X, y, hyperparameters_dict, feature_names, sample_names, hits_data, output_path):
+def train(rf, X, y, feature_names, sample_names, hits_data, output_path):
     original_feature_names = feature_names[:]
     original_X = X[:]
     logger.debug('\n'+'#'*100 + f'\nTrue labels:\n{y.tolist()}\n' + '#'*100 + '\n')
 
-    logging.info('Training...')
     # Fit the best model configuration on the WHOLE dataset
-    rf = RandomForestClassifier(**hyperparameters_dict)
+    logging.info('Training...')
     model = rf.fit(X, y)
     importance = model.feature_importances_
 
@@ -140,15 +145,16 @@ def train(X, y, hyperparameters_dict, feature_names, sample_names, hits_data, ou
     decreasing_feature_importance = np.argsort(importance)[::-1]
     assert (sorted(importance, reverse=True) == importance[decreasing_feature_importance]).all()
 
-    # sort data by feature importance permutation
-    # importance = importance[decreasing_feature_importance]
-    # feature_names = feature_names[decreasing_feature_importance]
-    # X = X[:, decreasing_feature_importance]
+    # the indexes that will be used in the next fitting process
+    # At first, we start with all features. Next we will remove less important once.
+    features_indexes_to_keep = range(len(feature_names))
 
+    # write feature importance to storage
     with open(f'{output_path}/feature_importance.txt', 'w') as f:
         for i in range(len(importance)):
             f.write(f'{feature_names[i]}\t{importance[i]}\n')
 
+    # write sorted feature importance to storage
     with open(f'{output_path}/sorted_feature_importance.txt', 'w') as f:
         for i in range(len(importance)):
             f.write(f'{feature_names[decreasing_feature_importance[i]]}\t'
@@ -168,7 +174,7 @@ def train(X, y, hyperparameters_dict, feature_names, sample_names, hits_data, ou
         # logger.info(f'Current model\'s predictions\n{predictions.tolist()}')
 
         # compute current model accuracy for each fold of the cross validation
-        cv_score = cross_val_score(model, X, y, cv=StratifiedKFold(n_splits=4), n_jobs=-1)
+        cv_score = cross_val_score(model, X, y, cv=StratifiedKFold(n_splits=4))
 
         # current model cv_avg_error_rate rate
         cv_avg_error_rate = 1 - cv_score.mean()
@@ -181,9 +187,9 @@ def train(X, y, hyperparameters_dict, feature_names, sample_names, hits_data, ou
             break
 
         # save current model (unsorted) features to a csv file
-        df = save_model_features(X, feature_names, sample_names, number_of_features, output_path)
+        df = save_model_features(original_X, features_indexes_to_keep, feature_names, sample_names, f'{output_path}/Top_{number_of_features}_features')
 
-        generate_heat_map(df, number_of_features, hits_data, number_of_samples, output_path)
+        generate_heat_map(df, number_of_features, hits_data, number_of_samples, f'{output_path}/{number_of_features}')
 
         generate_roc_curve(X, y, rf, number_of_features, output_path)
 
@@ -199,32 +205,17 @@ def train(X, y, hyperparameters_dict, feature_names, sample_names, hits_data, ou
             logger.info(f'Current model\'s predictions\n{predictions}')
             logger.info(f'number_of_features {number_of_features}')
             logger.info(f'output_path {output_path}')
-            # flag = 1
-
-        # model2 = joblib.load(os.path.join(output_path, f'Top_{number_of_features}_features_model.pkl'))
-        # predictions2 = model2.predict(X).tolist()
-        # assert (predictions2 == y).mean() == model2.score(X, y)
-        # logger.info(f'Full model2 error rate is {1 - (predictions2 == y).mean()} (for debugging)')
-        # if 1 - (predictions2 == y).mean() > cv_avg_error_rate:
-        #     logger.info(f'Current model\'s predictions\n{predictions2}')
-        #     logger.info(f'number_of_features {number_of_features}')
-        #     logger.info(f'output_path {output_path}')
-        #     flag = 1
-        #
-        # if flag:
-        #     exit()
 
         # update number of features
         number_of_features //= 2
 
         # extract only the (new) half most important features
-        features_to_keep = sorted(decreasing_feature_importance[:number_of_features])
-        feature_names = original_feature_names[features_to_keep]
-        X = original_X[:, features_to_keep]
+        features_indexes_to_keep = sorted(decreasing_feature_importance[:number_of_features])
+        feature_names = original_feature_names[features_indexes_to_keep]
+        X = original_X[:, features_indexes_to_keep]
 
         if number_of_features > 0:
             # re-evaluate
-            # rf = RandomForestClassifier(n_estimators=number_of_trees)
             model = rf.fit(X, y)
 
     return cv_avg_error_rates, number_of_features_per_model
@@ -235,8 +226,10 @@ def train_models(csv_file_path, num_of_configurations_to_sample, done_path, cv_n
     csv_folder, csv_file_name = os.path.split(csv_file_path)
     csv_file_prefix = os.path.splitext(csv_file_name)[0]  # without extension
     output_path = os.path.join(csv_folder, f'{csv_file_prefix}_model')
+    os.makedirs(output_path, exist_ok=True)
+
     best_model_path = os.path.join(output_path, f'best_model')
-    shutil.rmtree(best_model_path, ignore_errors=True)
+    # shutil.rmtree(best_model_path, ignore_errors=True)
 
     feature_selection_summary_path = f'{output_path}/feature_selection_summary.txt'
 
@@ -245,18 +238,38 @@ def train_models(csv_file_path, num_of_configurations_to_sample, done_path, cv_n
 
     X_train, y_train, X_test, y_test, feature_names, sample_names_train, sample_names_test = parse_data(csv_file_path)
 
+    # single feature analysis
+    logging.info('Applying single feature analysis...')
+    perfect_feature_names, perfect_feature_indexes = measure_each_feature_accuracy(X_train, y_train, feature_names, output_path)
+    if perfect_feature_names:
+        df = save_model_features(X_train, perfect_feature_indexes, perfect_feature_names, sample_names_train, f'{output_path}/perfect_feature_names')
+        generate_heat_map(df, df.shape[1], False, df.shape[0], f'{output_path}/perfect_feature_names')
+    else:
+        # touch a file so we can see that there were no perfect features
+        with open(f'{output_path}/perfect_feature_names', 'w') as f:
+            pass
+
+    # sanity check
+    # perfect_feature_names =['PMMILDLDHSQV', 'YASTIVVDLDHT', 'CDAALHVIAALD', 'CVSASMDLDHLE', 'CSVELCNISGYG', 'SSQIMFHVYN', 'CPVSPQSLDAPC', 'MSWIFYALDPYE', 'SSHISLLMSV', 'ATLKHYLIQDGI', 'CTWGGPGFDLFC', 'CPFDSAGFDPYC', 'CTAAFISGHYPD', 'SSFFYMQAYV', 'TPIETISDSVYK', 'LDSLHIHYLVSF', 'CHISHVQDLPYL', 'WLWYLASDGI', 'SINHGLLAYI', 'CIALLKEGEPFS', 'CSADNLNLYC', 'ACDPYDCRHLYS', 'WLAVLYADMVFY', 'WHWSLLQDTMYH', 'GTSSFPYSHLWA', 'CALGWLLDLDHC', 'CQAFIDPYEC', 'SLSQNYSLLMII', 'NGYDYFGLRF', 'GDPYEILVLSRC', 'IESGDPYEARFS', 'GYDLLRDRYI', 'PINDLDADGI', 'SAHIWLVTSI', 'CLRLHLASLTQC', 'SRMDFRYLLE', 'CPHSLIMLLGDL', 'WPVHLEHTNI', 'GPYELHYEDK', 'CRFGAPGFDVQC', 'LLSLVLMDSMYF', 'APTCFRGTDC', 'TWRSGNASGSP', 'DGYFAHFWSVLQ', 'CAVLLDPYEC', 'CIARKDMQATQN', 'CMVWLDNVLSLC', 'PFEALELMRT']
+    # for feature in perfect_feature_names:
+    #     rf = RandomForestClassifier(**{'n_estimators': 2000, 'max_features': 'auto', 'max_depth': 70, 'min_samples_split': 5, 'min_samples_leaf': 1, 'bootstrap': True, 'n_jobs': -1}).fit(df[feature].as_matrix().reshape(-1, 1), y_train)
+    #     rf = RandomForestClassifier(**configuration).fit(df[feature].as_matrix().reshape(-1, 1), y_train)
+    #     print(','.join(rf.predict(df[feature].as_matrix().reshape(-1, 1)).tolist()))
+    #     print(','.join(y_train.as_matrix().tolist()))
+
+    # feature selection analysis
+    logging.info('\nApplying feature selection analysis...')
     if cv_num_of_splits < 2:
-        logging.info('Number of CV folds is less than 2. Updating number of splits to number of samples and applying Leave One Out approach!')
+        logging.info('Number of CV folds is less than 2. '
+                     'Updating number of splits to number of samples and applying Leave One Out approach!')
         cv_num_of_splits = len(y_train)
     logging.info(f'Number of CV folds is {cv_num_of_splits}.')
 
     logger.info('\n'+'#'*100 + f'\nTrue labels:\n{y_train.tolist()}\n' + '#'*100 + '\n')
 
+    logging.info('Sampling hyperparameters...')
     hyperparameters_grid = get_hyperparameters_grid()
 
-    logging.info('Sampling hyperparameters...')
-
-    os.makedirs(output_path, exist_ok=True)
     feature_selection_summary_f = open(feature_selection_summary_path, 'w')
     feature_selection_summary_f.write(f'model_number\tnum_of_features\tfinal_error_rate\n')
 
@@ -264,37 +277,43 @@ def train_models(csv_file_path, num_of_configurations_to_sample, done_path, cv_n
     for i, configuration in enumerate(sampled_configurations):
         model_number = str(i).zfill(len(str(num_of_configurations_to_sample)))
         output_path_i = os.path.join(output_path, model_number)
-        logging.info('Creating output path...')
+        logging.info(f'Creating output path #{i}...')
         os.makedirs(output_path_i, exist_ok=True)
 
         save_configuration_to_txt_file(configuration, output_path_i)
 
         logging.info(f'Configuration #{i} hyper-parameters are:\n{configuration}')
-        errors, features = train(X_train, y_train, configuration, feature_names, sample_names_train, is_hits_data, output_path_i)
+        rf = RandomForestClassifier(**configuration)
+        errors, features = train(rf, X_train, y_train, feature_names, sample_names_train, is_hits_data, output_path_i)
 
         plot_error_rate(errors, features, cv_num_of_splits, output_path_i)
 
         feature_selection_summary_f.write(f'{model_number}\t{features[-1]}\t{errors[-1]}\n')
 
-        if features[-1]==1 and errors[-1]==0:
+        if features[-1] == 1 and errors[-1] == 0:
             # found the best model (accuracy-wise)
+            # there is no point to continue...
             break
+
     feature_selection_summary_f.close()
 
     # find who was the best performing model
     models_stats = pd.read_csv(feature_selection_summary_path, sep='\t', dtype={'model_number': str, 'num_of_features':int, 'final_error_rate': float })
     lowest_error_models = models_stats[models_stats['final_error_rate'] ==
                                        min(models_stats['final_error_rate'])]
+
+    # in case there is a tie between best models, we choose the model with the lowest number of features
     if len(lowest_error_models) > 1:
         lowest_error_models = lowest_error_models[lowest_error_models['num_of_features'] ==
                                                   min(lowest_error_models['num_of_features'])]
+
     best_model = lowest_error_models['model_number'].iloc[0]
 
+    # keep only best model (and remove the rest)
     shutil.copytree(f'{output_path}/{best_model}', best_model_path)
-
     for folder in os.listdir(output_path):
         path = f'{output_path}/{folder}'
-        if folder=='best_model' or not os.path.isdir(path):
+        if folder == 'best_model' or not os.path.isdir(path):
             continue
         shutil.rmtree(path)
 
@@ -302,10 +321,47 @@ def train_models(csv_file_path, num_of_configurations_to_sample, done_path, cv_n
         f.write(' '.join(argv) + '\n')
 
 
+def measure_each_feature_accuracy(X_train, y_train, feature_names, output_path):
+    feature_to_avg_accuracy = {}
+    # df = pd.read_csv(f'{output_path}/Top_149_features.csv', index_col='sample_name')
+    rf = RandomForestClassifier()
+
+    for i, feature in enumerate(feature_names):
+        # if i % 10 == 0:
+        logger.info(f'Checking feature {feature} number {i}')
+        # assert df.columns[i] == feature
+        cv_score = cross_val_score(rf, X_train[:, i].reshape(-1, 1), y_train, cv=StratifiedKFold(n_splits=4, shuffle=True)).mean()
+        if cv_score == 1:
+            logger.info('-' * 10 + f'{feature} has 100% accuracy!' + '-' * 10)
+        #     print(X_train[:, i].reshape(-1, 1).tolist()[:8] + X_train[:, i].reshape(-1, 1).tolist()[12:])
+        #     print(f'min of other class: {min(X_train[:, i].reshape(-1, 1).tolist()[:8] + X_train[:, i].reshape(-1, 1).tolist()[12:])}')
+        #     print(X_train[:, i].reshape(-1, 1).tolist()[8:12])
+        #     # print(y_train.tolist())
+        #     print('Accuracy is 1')
+        feature_to_avg_accuracy[feature] = cv_score
+
+    with open(f'{output_path}/single_feature_accuracy.txt', 'w') as f:
+        f.write('Feature\tAccuracy_on_cv\n')
+        for feature in sorted(feature_to_avg_accuracy, key=feature_to_avg_accuracy.get, reverse=True):
+            f.write(f'{feature}\t{feature_to_avg_accuracy[feature]}\n')
+
+    perfect_feature_names = []
+    perfect_feature_indexes = []
+    with open(f'{output_path}/features_with_perfect_accuracy.txt', 'w') as f:
+        for i, feature in enumerate(feature_to_avg_accuracy):
+            if feature_to_avg_accuracy[feature] == 1:
+                perfect_feature_names.append(feature)
+                perfect_feature_indexes.append(i)
+                f.write(f'{feature}\n')
+
+    return perfect_feature_names, perfect_feature_indexes
+
+
 def save_configuration_to_txt_file(sampled_configuration, output_path_i):
     with open(f'{output_path_i}/hyperparameters_configuration.txt', 'w') as f:
         for key in sampled_configuration:
             f.write(f'{key}={sampled_configuration[key]}\n')
+    joblib.dump(sampled_configuration, f'{output_path_i}/hyperparameters_configuration.pkl')
 
 
 if __name__ == '__main__':
@@ -329,4 +385,3 @@ if __name__ == '__main__':
     logger = logging.getLogger('main')
 
     train_models(args.data_path, args.num_of_configurations_to_sample, args.done_file_path, args.cv_num_of_splits, argv=sys.argv)
-
