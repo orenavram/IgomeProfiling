6fd2a4094ede35bd46cfe1ed0548f297afa029c8
yael mohaban <yael4391@gmail.com>
2020-12-21 13:11:29 +0200

    create a shell script that close the machines

--- tools/stop_machine_aws.py
+++ tools/stop_machine_aws.py
@@ -1,27 +1,32 @@
-def connect_ssh(host_ssh,username_ssh,password_ssh,private_key):
-    # Connect
-    print('connecting ssh...')
+import subprocess
+import paramiko
+
+def stop_machines(shell_script):
+    print('Stop all machines...')
+    sec=subprocess.call(['sh', shell_script])
+    if sec==0:
+        print('the machines are close')
+    else:
+        print('can\'t close the machines')    
+
+def stop_manager_machines(private_key):
+    print('Connecting to manager machine...')
+     # Connect
     key = paramiko.RSAKey.from_private_key_file(private_key)
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host_ssh, username=username_ssh, password=password_ssh,pkey=key)
-    print('connected')
-
-    return client
-
-def stop_machine(ssh_client):
-    print('stop all machine...')
-    
-
+    client.connect('35.160.134.140', username='ec2-user', password='',pkey=key)
+    command='sudo shutdown -h now'
+    stdin, stdout, stderr = ssh.exec_command(command)
+    lines = stdout.readlines()
+    print(lines)
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('host', type=str, help='The host name for connect to ssh')
-    parser.add_argument('user_name', type=str, help='The user name for connect ssh')
-    parser.add_argument('password', type=str, help='The password foe connect ssh')
-    parser.add_argument('key', type=str, help='A path for pem file of the private key')
+    parser.add_argument('shell_script', type=str, help='A path for the shell script that close all the machine')
+    #parser.add_argument('private_key', type=str, help='A path for the private key pem file')
     args = parser.parse_args()
 
-    client=connect_ssh(args.host,args.user_name,args.password,args.pkey)
-    stop_machine(client)
+    stop_machines(args.shell_script)
+    #stop_manager_machines(args.private_key)
