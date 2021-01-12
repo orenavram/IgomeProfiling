068c6eda16da3194800a792a1dbd4506200f5e43
yael mohaban <yael4391@gmail.com>
2021-01-12 12:11:02 +0200

    finish script stop_machine

--- tools/stop_machine_aws.py
+++ tools/stop_machine_aws.py
@@ -1,25 +1,39 @@
 import subprocess
 import paramiko
 import boto3 
+import os
+import sys
 
-def stop_machines():
+def stop(instance,f_log):
+    if instance.state['Name']!='stopped':
+            f_log.write("Id: {0}, Platform: {1},Type: {2}, Public IPv4: {3}, AMI: {4}, State: {5}\n".format(instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state))
+            instance.stop()
+            f_log.write('After stop function:\n')
+            f_log.write("Id: {0}, Platform: {1}, Type: {2}, Public IPv4: {3}, AMI: {4}, State: {5}\n".format(instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state))
+
+def stop_machines(log_path,type_machine):
     print('Stop all machines...')
-    region='us-west-2'
-    #ec2 = boto3.resource('ec2',region)
-    #ids = ['i-03df2ae895498b6da']
-    #ec2.instances.filter(InstanceIds = ids).stop() #for stopping an ec2 instance
-    
+    f_log=open(log_path,'w')
     ec2 = boto3.resource('ec2')
     for instance in ec2.instances.all():
-        print("Id: {0}\nPlatform: {1}\nType: {2}\nPublic IPv4: {3}\nAMI: {4}\nState: {5}\n".format(instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state))    
-    #client = boto3.client('ec2',region_name=region, endpoint_url=f'https://sts.{region}.amazonaws.com')
-    #response = client.stop_instances(InstanceIds=['i-03df2ae895498b6da'])
-    #Hibernate=True|False,
-    #DryRun=True|False,
-    #Force=True|False
-    #instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-    #for instance in instances:
-    #    print(instance.id, instance.instance_type)
+        #print("Id: {0}\nPlatform: {1}\nType: {2}\nPublic IPv4: {3}\nAMI: {4}\nState: {5}\n".format(instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state))    
+        if type_machine: 
+            if instance.state['Name']!='stopped' and instance.instance_type==type_machine:
+                stop(instance,f_log)
+        else:
+            if instance.state['Name']!='stopped':
+                stop(instance,f_log)
+
+                
+    f_log.close()
 
 if __name__ == "__main__":    
-    stop_machines()
+    print(f'Starting {sys.argv[0]}. Executed command is:\n{" ".join(sys.argv)}')
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('log_path', help='A path to a txt file for log of the run')
+    parser.add_argument('--type_machine', help='stop only one type of machine')
+    args = parser.parse_args()
+    stop_machines(args.log_path,args.type_machine)
