3b0846fb0f893a6a14dfca9a7bd0da3dcee1d07f
--replace-all <yael4391@gmail.com>
2021-05-09 14:52:30 +0300

    validation of json file

--- tools/validation_files.py
+++ tools/validation_files.py
@@ -2,6 +2,21 @@ import sys
 from auxiliaries.pipeline_auxiliaries import load_table_to_dict
 import os
 import re
+import json
+import jsonschema
+from jsonschema import validate
+
+# Describe what kind of json you expect.
+sample2bc_Schema = {
+    "type": "object",
+    "propertyNames": {
+        "pattern": "^[A-Za-z0-9_]*$"
+    },
+    "patternProperties": {
+        "^[A-Za-z0-9_]*$" : { "type": "array", "items": {"type": "string", "pattern": "^[A-Za-z0-9_]*$"} } 
+    }
+}
+
 
 def is_same_samples(samples2bc_path, barcode2samples_path):
     # verify that file sample2biologicalcondition and file barcode2sample have the same samples.
@@ -9,7 +24,7 @@ def is_same_samples(samples2bc_path, barcode2samples_path):
     samples_from_barcode2sample = sorted(load_table_to_dict(barcode2samples_path,'Barcode {} belongs to more than one sample!!').values())
     return samples_from_sample2bc == samples_from_barcode2sample
 
-def is_valid_structure_file(file_path):
+def is_valid_txt_file(file_path):
     # verify the structure of the file -  should look like: word\tword\n
     regex = '\w*\t\w*\n'
     for data in  open(file_path, "rb"):
@@ -19,28 +34,38 @@ def is_valid_structure_file(file_path):
             return False
     return True        
 
+def is_valid_Json_file(jsonData):
+    try:
+        validate(instance=jsonData, schema=sample2bc_Schema)
+    except jsonschema.exceptions.ValidationError as err:
+        return False
+    return True
+
+
 def is_input_files_valid(samples2bc_path, barcode2samples_path):
     samples2bc_valid = False
+    barcode2samples_valid = False
+    # Test if the file samples2bc_path is valid. 
     if samples2bc_path:
         filename, file_extension = os.path.splitext(samples2bc_path)
-        samples2bc_valid = is_valid_structure_file(samples2bc_path) if file_extension=='.txt' else True
-        if samples2bc_valid: 
-            print(f'The structure of file name {samples2bc_path} is valid')
-        else:
+        samples2bc_valid = is_valid_txt_file(samples2bc_path) if file_extension == '.txt' else is_valid_Json_file(json.load(open(samples2bc_path)))
+        if  not samples2bc_valid: 
             print(f'The structure of file name {samples2bc_path} is not valid')   
-    barcode2samples_valid = is_valid_structure_file(barcode2samples_path) if barcode2samples_path else False
-    if barcode2samples_valid: 
-            print(f'The structure of file name {barcode2samples_path} is valid')
-    else:
+    
+    # Test if the file barcode2samples_valid is valid. 
+    if barcode2samples_path:
+        barcode2samples_valid = is_valid_txt_file(barcode2samples_path) 
+        if not barcode2samples_valid: 
             print(f'The structure of file name {barcode2samples_path} is not valid')   
-
-    if samples2bc_path and barcode2samples_path:
-        if samples2bc_valid and barcode2samples_valid:
-            same_samples = is_same_samples(samples2bc_path, barcode2samples_path)
-            if not same_samples:
-                print(f'The files: {samples2bc_path} and {barcode2samples_path} not contain the same samples!!')
-                return False
+    
+    # If there are two files - test that the two files have the same samples.
+    if (samples2bc_path and barcode2samples_path):
+        same_samples = is_same_samples(samples2bc_path, barcode2samples_path) if (samples2bc_valid and barcode2samples_valid) else True
+        if not same_samples:
+            print(f'The files: {samples2bc_path} and {barcode2samples_path} not contain the same samples!!')
+            return False
         else:
-            False
+            return False    
     else:
+        # input only one file - return the validation of the input file.
         return samples2bc_valid or barcode2samples_valid
