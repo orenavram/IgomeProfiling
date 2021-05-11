6575a67fa4646de7b081902aa22ad094d0612562
--replace-all <yael4391@gmail.com>
2021-05-11 11:05:11 +0300

    create new function for determine if to stop or not the instance.

--- tools/stop_machine_aws.py
+++ tools/stop_machine_aws.py
@@ -2,16 +2,22 @@ import boto3
 import datetime
 import re
 
-def stop(instance, logger):
-    if instance.state['Name'] != 'stopped':
-            instance.stop()
-            logger.info("Stop the instance - Id: {0}, Type: {1}, Public IPv4: {2}, State: {3}".format(instance.id, instance.instance_type, instance.public_ip_address, instance.state))
-
 def is_match_name_instance(names, instance):
     for name in names:
         if re.search(name, instance.tags[0]['Value']):
             return True
     return False
+
+def is_stop_instance(instance, types, names):
+    if instance.state['Name'] == 'stopped':
+        return False
+    if types and names:
+        return  instance.instance_type in types or is_match_name_instance(names, instance)
+    if types and instance.instance_type not in types:
+        return False
+    if names and not is_match_name_instance(names, instance):
+        return False
+    return True;    
 
 def stop_machines(type_machine, name_machines, logger):
     logger.info('Stop machines...')
@@ -26,7 +32,11 @@ def stop_machines(type_machine, name_machines, logger):
     names = name_machines.split(',') if name_machines else ''
 
     for instance in list_instances:
-        if not instance.state['Name'] == 'stopped' and (((types and names) and (instance.instance_type in types or is_match_name_instance(names, instance))) or ((not types or instance.instance_type in types) and (not names or is_match_name_instance(names, instance)))):
-            stop(instance, logger)
+        if is_stop_instance(instance, types, names):
+            try:
+                instance.stop()
+                logger.info("Stop the instance - Id: {0}, Type: {1}, Public IPv4: {2}, State: {3}".format(instance.id, instance.instance_type, instance.public_ip_address, instance.state))
+            except botocore.exceptions.ClientError as error:
+                logger.error(f'{datetime.datetime.now()}: Error {error} - can\'t stop the instance {instance.id}')
         else:
             logger.info(f'{datetime.datetime.now()}: Skipping on instance -  {instance.id} and not stop it.')
